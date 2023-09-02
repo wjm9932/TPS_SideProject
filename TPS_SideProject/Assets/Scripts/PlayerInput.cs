429dfe8eb6a1ee14a30bfe7c8e5dbd4bfa39f02b
Jaemin <wjm9932@gmail.com>
2023-09-02 16:48:39 -0700

    Change unity version

---
+++ TPS_SideProject/Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    public Vector2 moveInput { get; private set; }
+    public bool isJump { get; private set; }
+    public bool isFire { get; private set; }
+    public bool isReload { get; private set; }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+
+        if (moveInput.sqrMagnitude > 1f)
+        {
+            moveInput = moveInput.normalized;
+        }
+        isJump = Input.GetButtonDown("Jump");
+        isFire = Input.GetButton("Fire1");
+        isReload = Input.GetButtonDown("Reload");
+    }
+}
