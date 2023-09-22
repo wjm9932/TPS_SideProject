579ab2cfc8447b3540a254f4ef776b88135ad6c6
Jaemin <wjm9932@gmail.com>
2023-09-22 10:06:43 -0700

    Implement fire gun

--- TPS_SideProject/Assets/Scripts/Gun.cs
+++ TPS_SideProject/Assets/Scripts/Gun.cs
@@ -4,15 +4,181 @@ using UnityEngine;
 
 public class Gun : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public enum State
     {
-        
+        Ready,
+        Empty,
+        Reloading
     }
 
-    // Update is called once per frame
-    void Update()
+    public State state { get; private set; }
+
+    public AudioClip shotClip;
+    public AudioClip reloadClip;
+    public ParticleSystem muzzleFlashEffect;
+    public ParticleSystem shellEjectEffect;
+    public Transform fireTransform;
+    public Transform leftHandMount;
+
+    public float damage = 25f;
+    public float fireDistance = 100f;
+
+    public int ammoRemain = 100;
+    public int magAmmo;
+    public int magCapacity = 30;
+
+    public float timeBetFire = 0.12f;
+    public float reloadTime = 1.8f;
+
+    [Range(0f, 10f)] public float maxSpread = 3f;
+    [Range(1f, 10f)] public float stability = 1f;
+    [Range(0.01f, 10f)] public float restoreFromRecoilSpeed = 2f;
+
+    private PlayerShooter gunHolder;
+    private LineRenderer bulletLineRenderer;
+    private AudioSource gunAudioPlayer;
+
+
+    private float currentSpread;
+    private float currentSpreadVelocity;
+
+    private float lastFireTime;
+
+    private LayerMask excludeTarget;
+
+    private void Awake()
     {
-        
+        gunAudioPlayer = GetComponent<AudioSource>();
+        bulletLineRenderer = GetComponent<LineRenderer>();
+
+        bulletLineRenderer.positionCount = 2;
+        bulletLineRenderer.enabled = false;
+    }
+
+    public void SetUp(PlayerShooter gunHolder)
+    {
+        this.gunHolder = gunHolder;
+        excludeTarget = gunHolder.excludeTarget;
+    }
+    private void OnEnable()
+    {
+        magAmmo = magCapacity;
+        currentSpread = 0f;
+        lastFireTime = 0f;
+        state = State.Ready;
+    }
+    private void OnDisable()
+    {
+        StopAllCoroutines();
+    }
+
+    public bool Fire(Vector3 aimTarget)
+    {
+        if (state == State.Ready && Time.time >= lastFireTime + timeBetFire)
+        {
+            var fireDir = aimTarget - fireTransform.position;
+
+            var xError = Utility.GetRandomNormalDistribution(0f, currentSpread);
+            var yError = Utility.GetRandomNormalDistribution(0f, currentSpread);
+
+            fireDir = Quaternion.AngleAxis(yError, Vector3.up) * fireDir;
+            fireDir = Quaternion.AngleAxis(xError, Vector3.right) * fireDir;
+
+
+            currentSpread += 1f / stability;
+
+            lastFireTime = Time.time;
+            Shot(fireTransform.position, fireDir);
+
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    private void Shot(Vector3 startPoint, Vector3 direction)
+    {
+        RaycastHit hit;
+        Vector3 hitPosition;
+
+        if (Physics.Raycast(startPoint, direction, out hit, fireDistance, ~excludeTarget) == true)
+        {
+            var target = hit.collider.GetComponent<IDamageble>();
+
+            if (target != null)
+            {
+                DamageMessage damageMessage;
+                damageMessage.damager = gunHolder.gameObject;
+                damageMessage.amount = damage;
+                damageMessage.hitPoint = hit.point;
+                damageMessage.hitNormal = hit.normal;
+
+                target.ApplyDamage(damageMessage);
+            }
+            hitPosition = hit.point;
+        }
+        else
+        {
+            hitPosition = startPoint + direction * fireDistance;
+        }
+
+        StartCoroutine(ShotEffect(hitPosition));
+        --magAmmo;
+        if (magAmmo <= 0)
+        {
+            state = State.Empty;
+        }
+    }
+
+
+    private IEnumerator ShotEffect(Vector3 hitPosition)
+    {
+        muzzleFlashEffect.Play();
+        shellEjectEffect.Play();
+
+        gunAudioPlayer.PlayOneShot(shotClip);
+
+        bulletLineRenderer.enabled = true;
+        bulletLineRenderer.SetPosition(0, fireTransform.position);
+        bulletLineRenderer.SetPosition(1, hitPosition);
+
+        yield return new WaitForSeconds(0.03f);
+
+        bulletLineRenderer.enabled = false;
+    }
+
+    public bool Reload()
+    {
+        if (state == State.Reloading || ammoRemain <= 0 || magAmmo >= magCapacity)
+        {
+            return false;
+        }
+        else
+        {
+            StartCoroutine(ReloadRoutine());
+            return true;
+        }
+    }
+
+    private IEnumerator ReloadRoutine()
+    {
+        state = State.Reloading;
+        gunAudioPlayer.PlayOneShot(reloadClip);
+
+        yield return new WaitForSeconds(reloadTime);
+
+        var ammoToFill = Mathf.Clamp(magCapacity - magAmmo, 0, ammoRemain);
+        magAmmo += ammoToFill;
+        ammoRemain -= ammoToFill;
+
+        state = State.Ready;
+    }
+
+    private void Update()
+    {
+        currentSpread = Mathf.Clamp(currentSpread, 0, maxSpread);
+        currentSpread = Mathf.SmoothDamp(currentSpread, 0f, ref currentSpreadVelocity, 1f / restoreFromRecoilSpeed);
     }
 }
---
+++ TPS_SideProject/Assets/Scripts/PlayerShooter.cs
@@ -0,0 +1,143 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShooter : MonoBehaviour
+{
+    public enum AimState
+    {
+        Idle,
+        HipFire
+    }
+
+    public AimState aimState { get; private set; }
+
+    public LayerMask excludeTarget;
+    public Gun gun;
+    private PlayerInput playerInput;
+    private Animator playerAnimator;
+    private Camera playerCamera;
+
+    private Vector3 aimPoint;
+    private bool linedUp => !(Mathf.Abs(playerCamera.transform.eulerAngles.y - transform.eulerAngles.y) > 1f);
+    private bool hasEnoughDistance => !Physics.Linecast(transform.position + Vector3.up * gun.fireTransform.position.y, gun.transform.position, ~excludeTarget);
+
+    private void Awake()
+    {
+        if (excludeTarget != (excludeTarget | (1 << gameObject.layer)))
+        {
+            excludeTarget |= 1 << gameObject.layer;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerCamera = Camera.main;
+        playerInput = GetComponent<PlayerInput>();
+        playerAnimator = GetComponent<Animator>();
+    }
+
+    private void OnEnable()
+    {
+        gun.gameObject.SetActive(true);
+        gun.SetUp(this);
+    }
+    private void OnDisable()
+    {
+        gun.gameObject.SetActive(false);
+    }
+    private void FixedUpdate()
+    {
+        if (playerInput.isFire == true)
+        {
+            Shoot();
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        var angle = playerCamera.transform.eulerAngles.x;
+        if(angle > 270)
+        {
+            angle -= 360f;
+        }
+        angle = angle / -180f + 0.5f;
+        playerAnimator.SetFloat("Angle", angle);
+        
+        UpdateAimTarget();
+        
+        if (playerInput.isReload == true)
+        {
+            Reload();
+        }
+    }
+
+    public void Reload()
+    {
+        if (gun.Reload() == true)
+        {
+            playerAnimator.SetTrigger("Reload");
+        }
+    }
+
+    public void Shoot()
+    {
+        if (aimState == AimState.Idle)
+        {
+            if (linedUp == true)
+            {
+                aimState = AimState.HipFire;
+            }
+        }
+        else if (aimState == AimState.HipFire)
+        {
+            if (hasEnoughDistance == true)
+            {
+                if (gun.Fire(aimPoint) == true)
+                {
+                    playerAnimator.SetTrigger("Shoot");
+                }
+            }
+            else
+            {
+                aimState = AimState.Idle;
+            }
+        }
+    }
+
+    private void UpdateAimTarget()
+    {
+        RaycastHit hit;
+        var ray = playerCamera.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0f));
+
+        if (Physics.Raycast(ray, out hit, gun.fireDistance, ~excludeTarget) == true)
+        {
+            aimPoint = hit.point;
+
+            if (Physics.Linecast(gun.fireTransform.position, hit.point, out hit, ~excludeTarget) == true) // intersect
+            {
+                aimPoint = hit.point;
+            }
+        }
+        else
+        {
+            aimPoint = playerCamera.transform.position + playerCamera.transform.forward * gun.fireDistance;
+        }
+    }
+    private void OnAnimatorIK(int layerIndex)
+    {
+        if(gun == null || gun.state == Gun.State.Reloading)
+        {
+            return;
+        }
+        else
+        {
+            playerAnimator.SetIKPositionWeight(AvatarIKGoal.LeftHand, 1.0f);
+            playerAnimator.SetIKRotationWeight(AvatarIKGoal.LeftHand, 1.0f);
+
+            playerAnimator.SetIKPosition(AvatarIKGoal.LeftHand, gun.leftHandMount.position);
+            playerAnimator.SetIKRotation(AvatarIKGoal.LeftHand, gun.leftHandMount.rotation);
+        }
+    }
+}
