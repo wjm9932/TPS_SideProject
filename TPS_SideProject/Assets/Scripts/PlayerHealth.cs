9e5c181b9014ce04cf083a7100b2eefc616aaaed
Jaemin <wjm9932@gmail.com>
2023-10-14 09:50:46 -0700

    Start making enemy

---
+++ TPS_SideProject/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+
+public class PlayerHealth : LivingEntity
+{
+    private Animator animator;
+    private AudioSource playerAudioPlayer;
+
+    public AudioClip deathClip;
+    public AudioClip hitClip;
+
+
+    private void Awake()
+    {
+        playerAudioPlayer = GetComponent<AudioSource>();
+        animator = GetComponent<Animator>();
+    }
+
+    protected override void OnEnable()
+    {
+        base.OnEnable();
+        UpdateUI();
+    }
+
+    public override void RestoreHealth(float newHealth)
+    {
+        base.RestoreHealth(newHealth);
+    }
+
+    private void UpdateUI()
+    {
+        UIManager.Instance.UpdateHealthText(dead ? 0f : health);
+    }
+
+    public override bool ApplyDamage(DamageMessage damageMessage)
+    {
+        if (!base.ApplyDamage(damageMessage))
+        {
+            return false;
+        }
+        else
+        {
+            EffectManager.Instance.PlayHitEffect(damageMessage.hitPoint, damageMessage.hitNormal, transform, EffectManager.EffectType.Flesh);
+            playerAudioPlayer.PlayOneShot(hitClip);
+            UpdateUI();
+            return true;
+        }
+    }
+
+    public override void Die()
+    {
+        base.Die();
+        playerAudioPlayer.PlayOneShot(deathClip);
+        animator.SetTrigger("Die");
+        UpdateUI();
+    }
+}
