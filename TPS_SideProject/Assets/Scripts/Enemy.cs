b5a4452197eecaee8097689e8ecb34197ae71b45
Jaemin <wjm9932@gmail.com>
2023-10-15 12:21:18 -0700

    AI

---
+++ TPS_SideProject/Assets/Scripts/Enemy.cs
@@ -0,0 +1,192 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class Enemy : LivingEntity
+{
+    private enum State
+    {
+        Patrol,
+        Tracking,
+        AttackBegin,
+        Attacking
+    }
+
+    private State state;
+
+    private NavMeshAgent agent;
+    private Animator animator;
+
+    public Transform attackRoot;
+    public Transform eyeTransform;
+
+    private AudioSource audioPlayer;
+    public AudioClip hitClip;
+    public AudioClip deathClip;
+
+    private Renderer skinRenderer;
+
+    public float runSpeed = 10f;
+    [Range(0.01f, 2f)] public float turnSmoothTime = 0.1f;
+    private float turnSmoothVelocity;
+
+    public float damage = 30f;
+    public float attackRadius = 2f;
+    private float attackDistance;
+
+    public float fieldOfView = 50f;
+    public float viewDistance = 10f;
+    public float patrolSpeed = 3f;
+
+    public LivingEntity targetEntity;
+    public LayerMask whatIsTarget;
+
+
+    private RaycastHit[] hits = new RaycastHit[10];
+    private List<LivingEntity> lastAttackedTargets = new List<LivingEntity>();
+
+    private bool hasTarget => targetEntity != null && !targetEntity.dead;
+
+
+#if UNITY_EDITOR
+
+    private void OnDrawGizmosSelected()
+    {
+        if(attackRoot != null)
+        {
+            Gizmos.color = new Color(1f, 0f, 0f, 0.5f);
+            Gizmos.DrawSphere(attackRoot.position, attackRadius);
+        }
+
+        if(eyeTransform != null)
+        {
+            var leftEyeRotation = Quaternion.AngleAxis(-fieldOfView * 0.5f, Vector3.up);
+            var leftRayDirection = leftEyeRotation * transform.forward;
+            Handles.color = new Color(1f, 1f, 1f, 0.2f);
+            Handles.DrawSolidArc(eyeTransform.position, Vector3.up, leftRayDirection, fieldOfView, viewDistance);
+        }
+    }
+
+#endif
+
+    private void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
+        audioPlayer = GetComponent<AudioSource>();
+        skinRenderer = GetComponentInChildren<Renderer>();
+
+        var attackPivot = attackRoot.position;
+        attackPivot.y = transform.position.y;
+        attackDistance = Vector3.Distance(transform.position, attackPivot) + attackRadius;
+
+        agent.stoppingDistance = attackDistance;
+        agent.speed = patrolSpeed;
+    }
+
+    public void Setup(float health, float damage, float runSpeed, float patrolSpeed, Color skinColor)
+    {
+        this.startingHealth = health;
+        this.health = health;
+        this.damage = damage;
+        this.runSpeed = runSpeed;
+        this.patrolSpeed = patrolSpeed;
+
+        skinRenderer.material.color = skinColor;
+
+        agent.speed = patrolSpeed;
+    }
+
+    private void Start()
+    {
+        StartCoroutine(UpdatePath());
+    }
+
+    private void Update()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        if (dead) return;
+    }
+
+    private IEnumerator UpdatePath()
+    {
+        while (!dead)
+        {
+            if (hasTarget)
+            {
+                if(state == State.Patrol)
+                {
+                    state = State.Tracking;
+                    agent.speed = runSpeed;
+                }
+
+                agent.SetDestination(targetEntity.transform.position);
+            }
+            else
+            {
+                if (targetEntity != null) targetEntity = null;
+                if(state != State.Patrol)
+                {
+                    state = State.Patrol;
+                    agent.speed = patrolSpeed;
+                }
+
+                if (agent.remainingDistance <= 1f)
+                {
+                    var patrolTargetPosition = Utility.GetRandomPointOnNavMesh(transform.position, 20f, NavMesh.AllAreas);
+                    agent.SetDestination(patrolTargetPosition);
+                }
+            }
+
+            yield return new WaitForSeconds(0.2f);
+        }
+    }
+
+    public override bool ApplyDamage(DamageMessage damageMessage)
+    {
+        if (!base.ApplyDamage(damageMessage)) return false;
+
+        return true;
+    }
+
+    public void BeginAttack()
+    {
+        state = State.AttackBegin;
+
+        agent.isStopped = true;
+        animator.SetTrigger("Attack");
+    }
+
+    public void EnableAttack()
+    {
+        state = State.Attacking;
+
+        lastAttackedTargets.Clear();
+    }
+
+    public void DisableAttack()
+    {
+        state = State.Tracking;
+
+        agent.isStopped = false;
+    }
+
+    private bool IsTargetOnSight(Transform target)
+    {
+        return false;
+    }
+
+    public override void Die()
+    {
+
+    }
+}
